89145dacb6636fa55189bd80ea6b7e8ee77bbc05
Dan Bright <dan@uplandsdynamic.com>
2024-06-17 20:03:11 +0100

    Add blurb

--- merge_spreadsheets.py
+++ merge_spreadsheets.py
@@ -1,3 +1,27 @@
+"""
+Author: Dan Bright, cosmoid@tutu.io
+License: GPLv3.0
+Version: 1.0
+First published: 17 June 2024
+Description: 
+    - A script to merge multiple excel spreadsheets into 
+    into a single primary spreadsheet.
+    - Values in primary sheet are overwritten with values 
+    from additional spreadsheets (in the order they were loaded)
+    - Rows are associated by the index column (INDEX_COL_NAME)
+    - All spreadsheets need to share the same index column name
+      (for associating rows)
+    - Either all columns, or only those existing in the primary 
+    sheet care merged, depending on the configuration.
+Usage:
+    1) Define config parameters in main()
+        - If 'COPY_ALL' is configured to True, all columns in 
+        additional spreadsheets are copied. If False, only those 
+        columns present in the primary sheet are copied.
+    2) Drop all spreadsheets into the directory at SHEET_DIR_PATH
+    3) Run script: python merge_spreadsheets.py
+"""
+
 import pandas as pd
 from pathlib import Path
 from pprint import pp
@@ -10,7 +34,8 @@ class MergeSpreadsheets:
                  primary_sheet_name: str = '',
                  output_dir: str = '',
                  output_fn: str = '',
-                 index_col_name: str = '',) -> None:
+                 index_col_name: str = '',
+                 copy_all:bool = False) -> None:
         self.sheet_dir_path = sheet_dir_path
         self.primary_sheet_name: str = primary_sheet_name
         self.output_dir: str = output_dir
@@ -19,6 +44,7 @@ class MergeSpreadsheets:
         self.dfs: list[dict] = []
         self.merged: pd.DataFrame = None
         self.primary_df: pd.DataFrame = None
+        self.copy_all:bool = copy_all
 
     def _read_sheets(self) -> None:
         df: pd.DataFrame = pd.DataFrame()
@@ -45,9 +71,9 @@ class MergeSpreadsheets:
                 self.index_col_name).to_dict('index')
             for index, updates in update_dict.items():
                 for col, val in updates.items():
-                    if col in self.primary_df.columns:
+                    if col in self.primary_df.columns or self.copy_all:
                         self.primary_df.loc[self.primary_df[self.index_col_name]
-                                            == index, col] = val
+                                        == index, col] = val
         self.primary_df.sort_values(by=self.index_col_name, inplace=True)
 
     def _write_output(self) -> None:
@@ -67,12 +93,13 @@ class MergeSpreadsheets:
 def main() -> None:
     SHEET_DIR_PATH = '/Users/dan/Dev/scu/InformationExtraction/data'
     OUTPUT_DIR = '/Users/dan/Dev/scu/InformationExtraction/output/merged'
-    PRIMARY_SHEET_NAME = 'WIP_VERSION_3d_DB.xlsx'
+    PRIMARY_SHEET_NAME = 'WIP_VERSION_3d_DB_colour_coded.xlsx'
     OUTPUT_FN = PRIMARY_SHEET_NAME
     INDEX_COL_NAME = 'RecNum'
+    COPY_ALL = True
 
     merge = MergeSpreadsheets(sheet_dir_path=SHEET_DIR_PATH, output_dir=OUTPUT_DIR,
-                              output_fn=OUTPUT_FN, primary_sheet_name=PRIMARY_SHEET_NAME, index_col_name=INDEX_COL_NAME)
+                              output_fn=OUTPUT_FN, primary_sheet_name=PRIMARY_SHEET_NAME, index_col_name=INDEX_COL_NAME, copy_all=COPY_ALL)
     merge.exe()
 
 
